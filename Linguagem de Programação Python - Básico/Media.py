36e46ad7e43978b4b205733047e89e45c084d3d5
Ronaldo Queiroz <ronquesil@gmail.com>
2023-09-16 22:22:47 -0300

    Update Media.py

---
+++ Linguagem de Programação Python - Básico/Media.py
@@ -0,0 +1,11 @@
+notaA = float(input("Informe a primeira nota: "))
+notaB = float(input("Informe a segunda nota: "))
+
+#calcular média
+mediaFinal = (notaA + notaB) / 2
+
+#verificação
+if mediaFinal >= 7.0:
+    print("A Média: %.1f - Aprovado "% mediaFinal)
+else:
+    print("A Média %.1f = Reprovado "% mediaFinal)
