4a84d048952bb7af1e626bfc3e608c478c130c58
Ronaldo Queiroz <ronquesil@gmail.com>
2023-11-26 16:52:43 -0300

    Update ExibirVariavel.py

--- Python_Basico/ExibirVariavel.py
+++ Python_Basico/ExibirVariavel.py
@@ -1,9 +1,9 @@
 codigo = 105
 nome = "José Santana"
 salario = 1650.00
-ativo= True
+ativo = True
 
-print("Código: %d "% codigo)
-print("Nome: %s "% nome)
-print("Salário: %.2f "% salario)
-print("Ativo: %r " % ativo)
+print("Código: %d "% codigo) #Exibe um valor inteiro.
+print("Nome: %s "% nome) #Exibe uma cadeia de caracteres (string).
+print("Salário: %.2f "% salario) #Exibe um valor decimal. (2 casas)
+print("Ativo: %r " % ativo) #Exibe true ou false (verdadeiro ou falso).
