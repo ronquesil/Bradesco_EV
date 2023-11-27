2e3bcbd8c025b1b2b7053c16648270952ab1c381
Ronaldo Queiroz <ronquesil@gmail.com>
2023-11-26 22:44:49 -0300

    Create ExibirVariavel2.py

---
+++ Python_Basico/ExibirVariavel2.py
@@ -0,0 +1,9 @@
+codigo = int(input("Digite o código do funcionário "))
+nome = input("Digite o nome do funcionário ")
+salario = float(input("Informe o salário "))
+ativo = True
+
+print("Código: %d "% codigo) #Exibe um valor inteiro.
+print("Nome: %s "% nome) #Exibe uma cadeia de caracteres (string).
+print("Salário: %.2f "% salario) #Exibe um valor decimal. (2 casas)
+print("Ativo: %r " % ativo) #Exibe true ou false (verdadeiro ou falso).
