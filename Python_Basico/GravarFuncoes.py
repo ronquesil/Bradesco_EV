825201aae516edc41f24a1955af2f134baa3a561
Ronaldo Queiroz <ronquesil@gmail.com>
2023-11-28 21:19:57 -0300

    Update GravarFuncoes.py

--- Python_Basico/GravarFuncoes.py
+++ Python_Basico/GravarFuncoes.py
@@ -3,3 +3,9 @@ arquivo = open('arqText.txt', 'w')
 arquivo.write('Curso Python \n')
 arquivo.write('Aula Prática')
 arquivo.close()
+
+#leitura do arquivo texto
+
+leitura=open('arqText', 'r')
+print(leitura.read())
+leitura.close()
